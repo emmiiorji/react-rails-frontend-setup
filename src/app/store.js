1dd660924c1d8120a28eb81f0260f93db827f994
emmiiorji <emmanuelorji2013@gmail.com>
2023-03-09 22:43:17 +0100

    Create the store and greetingSlice

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import greetingReducer from './components/greeting/greetingSlice';
 
 const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    greetings: greetingReducer,
   },
 });
 
